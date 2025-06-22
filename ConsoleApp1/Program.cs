92e20877314a8f60f7e050e9c0a299163168de31
mohamed gamal <mohamed.gamalabuy@gmail.com>
2025-06-22 22:22:37 +0300

    Q1 solved

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1
+
+            // Write a program that allows the user to enter a number then print it.
+
+            Console.WriteLine("pleas insert number!");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You number is : {number}");
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
