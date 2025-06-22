bbb37764600ef0798edd19adde0bbdd5765221e2
mohamed gamal <mohamed.gamalabuy@gmail.com>
2025-06-22 23:24:32 +0300

    Q2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,36 +4,42 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ConsoleApp1
 {
+    using System;
     internal class Program
     {
         static void Main(string[] args)
         {
 
             #region Q1
-
-            // Write a program that allows the user to enter a number then print it.
-
+               /*
+           Write a program that allows the user to enter a number then print it.
             Console.WriteLine("pleas insert number!");
+            
             int number = int.Parse(Console.ReadLine());
-            Console.WriteLine($"You number is : {number}");
+            Console.WriteLine($"Your number is : {number}");
+*/
+            #endregion
 
+            #region Q2
+            /* 
+            Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen 
+             string str = "123dftg";
+
+              int output;
+              if (int.TryParse(str, out output))
+              {
+                  Console.WriteLine("your number is: " + output);
+              }
+              else
+              {
+                  Console.WriteLine("*error* there is charater in your input cannout convert it to a intger");
+              }*/
             #endregion
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
+
+
     }
 }
